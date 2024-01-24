684aeade12242ef810995f20f0f57c517f231a71
Akinniyi Tolulope <47461817+Joshuafreemant@users.noreply.github.com>
2024-01-24 16:14:25 +0100

    admin interface and backend

--- alumni_app/src/pages/Admin/Signup.js
+++ alumni_app/src/pages/Admin/Signup.js
@@ -1,25 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { getFetch, postFetch } from "../apiCalls";
+import { postFetch } from "../../apiCalls";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-const Signup = () => {
+const AdminSignup = () => {
   const navigate = useNavigate();
-  const [allSets, setAllSets] = useState();
   const [userInfo, setUserInfo] = useState({
     firstname: "",
     lastname: "",
     email: "",
     set: "",
     house: "",
-    password: "",
+    password: ""
+   
   });
   const [loading, setLoading] = useState(false);
-  useEffect(() => {
-    getFetch(`set/all-set`).then((response) => {
-      setAllSets(response?.data);
-    });
-  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -31,6 +26,8 @@ const Signup = () => {
       set: userInfo.set,
       house: userInfo.house,
       password: userInfo.password,
+      role: "admin",
+      status: "active",
     })
       .then((response) => {
         setLoading(false);
@@ -114,7 +111,7 @@ const Signup = () => {
             <div className="flex justify-between gap-3">
               <div className="flex flex-col mb-3 w-full">
                 <label className="text-base text-white mb-1">Set</label>
-                <div className="pr-3 w-full bg-white rounded-md  flex items-center">
+                <div className="pr-3 w-full bg-white rounded-md p-3 flex items-center">
                   <select
                     onChange={(e) =>
                       setUserInfo({
@@ -122,21 +119,48 @@ const Signup = () => {
                         set: e.target.value,
                       })
                     }
-                    className="w-full p-3 text-base  rounded-md bg-white border-none outline-none"
+                    className="w-full  text-base  rounded-md bg-white border-none outline-none"
                   >
                     <option>Choose your Set</option>
-                    {allSets?.map((item) => (
-                      <option key={item._id} value={item.set}>
-                        {item.set}
-                      </option>
-                    ))}
+                    <option value="1987-1992">1987-1992</option>
+                    <option value="1988-1993">1988-1993</option>
+                    <option value="1989-1994">1989-1994</option>
+                    <option value="1990-1995">1990-1995</option>
+                    <option value="1991-1996">1991-1996</option>
+                    <option value="1992-1997">1992-1997</option>
+                    <option value="1993-1998">1993-1998</option>
+                    <option value="1994-1999">1994-1999</option>
+                    <option value="1995-2000">1995-2000</option>
+                    <option value="1996-2001">1996-2001</option>
+                    <option value="1997-2002">1997-2002</option>
+                    <option value="1998-2003">1998-2003</option>
+                    <option value="1999-2004">1999-2004</option>
+                    <option value="2000-2005">2000-2005</option>
+                    <option value="2001-2006">2001-2006</option>
+                    <option value="2002-2007">2002-2007</option>
+                    <option value="2003-2008">2003-2008</option>
+                    <option value="2004-2009">2004-2009</option>
+                    <option value="2005-2010">2005-2010</option>
+                    <option value="2006-2011">2006-2011</option>
+                    <option value="2007-2012">2007-2012</option>
+                    <option value="2008-2013">2008-2013</option>
+                    <option value="2009-2014">2009-2014</option>
+                    <option value="2010-2015">2010-2015</option>
+                    <option value="2011-2016">2011-2016</option>
+                    <option value="2012-2017">2012-2017</option>
+                    <option value="2013-2018">2013-2018</option>
+                    <option value="2014-2019">2014-2019</option>
+                    <option value="2015-2020">2015-2020</option>
+                    <option value="2016-2021">2016-2021</option>
+                    <option value="2017-2022">2017-2022</option>
+                    <option value="2018-2023">2018-2023</option>
                   </select>
                 </div>
               </div>
 
               <div className="flex flex-col mb-3 w-full">
                 <label className="text-base text-white mb-1">House</label>
-                <div className="pr-3 w-full bg-white rounded-md  flex items-center">
+                <div className="pr-3 w-full bg-white rounded-md p-3 flex items-center">
                   <select
                     onChange={(e) =>
                       setUserInfo({
@@ -144,13 +168,13 @@ const Signup = () => {
                         house: e.target.value,
                       })
                     }
-                    className="w-full p-3  text-base rounded-md bg-white border-none outline-none"
+                    className="w-full  text-base rounded-md bg-white border-none outline-none"
                   >
                     <option>Choose your House</option>
-                    <option value="Green">Green (Adeoti)</option>
-                    <option value="Pink">Pink (Adio)</option>
-                    <option value="Red">Red (Olabande)</option>
-                    <option value="Yellow">Yellow (Oladuuni)</option>
+                    <option value="Pink">Pink</option>
+                    <option value="Yellow">Yellow</option>
+                    <option value="Red">Red</option>
+                    <option value="Green">Green</option>
                   </select>
                 </div>
               </div>
@@ -171,7 +195,9 @@ const Signup = () => {
             </div>
             <div className="py-2 rounded-md mt-4 bg-[#1560bd] flex items-center w-full justify-center">
               {loading ? (
-                <p className="text-base m-0 text-white ">Loading...</p>
+                <p className="text-base m-0 text-white ">
+                  Loading...
+                </p>
               ) : (
                 <button className="text-base m-0 text-white ">Register</button>
               )}
@@ -192,4 +218,4 @@ const Signup = () => {
   );
 };
 
-export default Signup;
+export default AdminSignup;
